6613beaeec1a557737cca64a641c576031f01219
raf0670 <swagbondhu@gmail.com>
2026-03-04 13:20:57 +0600

    appends all words upon click

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,33 @@ const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all").then(res => res.json()).then(json => displayLessons(json.data));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url).then(res => res.json()).then(json => displayLevelWord(json.data));
+};
+
+const displayLevelWord = (words) => {
+    // get the word container and empty
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        // create a word card
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
+            <h2 class="font-bold text-3xl">${word.word}</h2>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <h2 class="text-3xl bangla">"${word.meaning} / ${word.pronunciation}"</h2>
+        </div>
+        `
+
+        // appending in the container
+        wordContainer.append(card);
+    });
+};
+
 const displayLessons = (lessons) => {
     // get the container and empty
     const levelConatiner = document.getElementById("level-container")
@@ -15,7 +42,7 @@ const displayLessons = (lessons) => {
         // create element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>
         `
 
         // appending element in the container
